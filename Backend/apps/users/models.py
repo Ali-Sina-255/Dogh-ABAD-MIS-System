fc1908ae2d074ec9e0a41a76a05f0aec388faf11
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-12-09 21:22:56 +0430

    user models is created

--- Backend/apps/users/models.py
+++ Backend/apps/users/models.py
@@ -1,3 +1,88 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, first_name, last_name, email, password=None):
+        if not email:
+            raise ValueError("User must have an email address!")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, first_name, last_name, email, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    Admin = 0
+    Reception = 2
+    ROLE_CHOICES = (
+        (Reception, "Reception"),
+        (Admin, "Admin"),
+    )
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    # The field used for user login
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_role(self):
+        if self.role == self.Reception:
+            return "Reception"
+        else:
+            return "Admin"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_pic = models.ImageField(
+        upload_to="user/profile_picture",
+        blank=True,
+        null=True,
+    )
+    address = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.user.email
